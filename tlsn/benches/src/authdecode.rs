1eea19f33559854a0da6918d1dfcc8a061506ef7
rpm <ryan.macarthur@gmail.com>
2024-02-25 21:30:55 -0400

    lint

--- tlsn/benches/src/authdecode.rs
+++ tlsn/benches/src/authdecode.rs
@@ -1,69 +1,67 @@
-
 use authdecode::{
     backend::{
         halo2,
         halo2::{
-            prover::Prover as Halo2ProverBackend, verifier::Verifier as Halo2VerififerBackend,
+            prover::Prover as Halo2ProverBackend,
+            verifier::Verifier as Halo2VerififerBackend,
             onetimesetup::OneTimeSetup,
         },
-        mock::{MockProverBackend, MockVerifierBackend},
     },
-    encodings::{ActiveEncodings, Encoding, FullEncodings, ToActiveEncodings},
+    encodings::{ ActiveEncodings, Encoding, FullEncodings, ToActiveEncodings },
     prover::{
         backend::Backend as ProverBackend,
         error::ProverError,
-        prover::{ProofInput, Prover},
-        InitData, ToInitData,
+        prover::{ ProofInput, Prover },
+        InitData,
+        ToInitData,
     },
-    utils::{choose, u8vec_to_boolvec},
-    verifier::{backend::Backend as VerifierBackend, verifier::Verifier},
+    utils::{ choose, u8vec_to_boolvec },
+    verifier::verifier::Verifier,
 };
 
 use std::env;
-use tracing_subscriber::{fmt::format::FmtSpan, EnvFilter};
+use tracing_subscriber::{ fmt::format::FmtSpan, EnvFilter };
 use hex::encode;
 use num::BigUint;
-use rand::{Rng, SeedableRng};
+use rand::{ Rng, SeedableRng };
 use rand_chacha::ChaCha12Rng;
 const PLAINTEXT_SIZE: usize = 1000;
 
-
 struct DummyEncodingsVerifier {}
-    impl authdecode::prover::EncodingVerifier for DummyEncodingsVerifier {
-        fn init(&self, init_data: InitData) {}
+impl authdecode::prover::EncodingVerifier for DummyEncodingsVerifier {
+    fn init(&self, init_data: InitData) {}
 
-        fn verify(
-            &self,
-            _encodings: &FullEncodings,
-        ) -> Result<(), authdecode::prover::EncodingVerifierError> {
-            Ok(())
-        }
+    fn verify(
+        &self,
+        _encodings: &FullEncodings
+    ) -> Result<(), authdecode::prover::EncodingVerifierError> {
+        Ok(())
     }
-
+}
 
 #[tokio::main]
 async fn main() {
-    tracing_subscriber::fmt()
+    tracing_subscriber
+        ::fmt()
         .with_env_filter(EnvFilter::from_default_env())
         .with_span_events(FmtSpan::NEW | FmtSpan::CLOSE)
         .init();
-        env::set_var("RAYON_NUM_THREADS", "1");
-        let mut rng = ChaCha12Rng::from_seed([0; 32]);
+    env::set_var("RAYON_NUM_THREADS", "1");
+    let mut rng = ChaCha12Rng::from_seed([0; 32]);
 
-        let plaintext: Vec<u8> = core::iter::repeat_with(|| rng.gen::<u8>())
+    let plaintext: Vec<u8> = core::iter
+        ::repeat_with(|| rng.gen::<u8>())
         .take(PLAINTEXT_SIZE)
         .collect();
 
-        let full_encodings: Vec<[u128; 2]> = core::iter::repeat_with(|| rng.gen::<[u128; 2]>())
+    let full_encodings: Vec<[u128; 2]> = core::iter
+        ::repeat_with(|| rng.gen::<[u128; 2]>())
         .take(PLAINTEXT_SIZE * 8)
         .collect();
     let full_encodings = full_encodings
         .into_iter()
         .map(|pair| {
-            [
-                Encoding::new(BigUint::from(pair[0])),
-                Encoding::new(BigUint::from(pair[1])),
-            ]
+            [Encoding::new(BigUint::from(pair[0])), Encoding::new(BigUint::from(pair[1]))]
         })
         .collect::<Vec<_>>();
     let full_encodings = FullEncodings::new(full_encodings);
@@ -75,31 +73,25 @@ async fn main() {
     let proving_key = OneTimeSetup::proving_key(params.clone());
     let verification_key = OneTimeSetup::verification_key(params);
 
-
-
     let h2proverBackend = Halo2ProverBackend::new(proving_key);
     let h2verifierBackend = Halo2VerififerBackend::new(verification_key);
 
     let prover = Prover::new(Box::new(h2proverBackend));
     let verifier = Verifier::new(Box::new(h2verifierBackend));
 
-
     let (prover, commitments) = prover.commit(vec![(plaintext, active_encodings)]).unwrap();
 
     let (verifier, verification_data) = verifier
-    .receive_commitments(
-        commitments,
-        vec![full_encodings.clone()],
-        InitData::new(vec![1u8; 100]),
-    )
-    .unwrap();
+        .receive_commitments(
+            commitments,
+            vec![full_encodings.clone()],
+            InitData::new(vec![1u8; 100])
+        )
+        .unwrap();
 
-let prover = prover
-    .check(verification_data, DummyEncodingsVerifier {})
-    .unwrap();
+    let prover = prover.check(verification_data, DummyEncodingsVerifier {}).unwrap();
 
-let (prover, proof_sets) = prover.prove().unwrap();
+    let (prover, proof_sets) = prover.prove().unwrap();
 
-let verifier = verifier.verify(proof_sets).unwrap();
-
+    let verifier = verifier.verify(proof_sets).unwrap();
 }
